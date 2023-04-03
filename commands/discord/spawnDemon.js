8294dc13655b720ef8f99b56b3a70ad4f52626c9
Abdul Karriem <AbdulKirby@gmail.com>
2023-04-03 08:55:54 -0500

    Basic Button Menu UI for Demon Encounter

--- commands/discord/spawnDemon.js
+++ commands/discord/spawnDemon.js
@@ -21,17 +21,50 @@ module.exports = {
 		const row = new ActionRowBuilder()
 			.addComponents(
 				new ButtonBuilder()
-					.setCustomId('primary')
+					.setCustomId('attack')
+					.setLabel('Attack')
+					.setStyle(ButtonStyle.Danger),
+				
+				new ButtonBuilder()
+					.setCustomId('talk')
 					.setLabel('Talk')
 					.setStyle(ButtonStyle.Primary),
+				
+				new ButtonBuilder()
+					.setCustomId('items')
+					.setLabel('Item')
+					.setStyle(ButtonStyle.Secondary),
+				
+				new ButtonBuilder()
+					.setCustomId('flee')
+					.setLabel('Flee')
+					.setStyle(ButtonStyle.Success),
 			);
 		
 		const rowD = new ActionRowBuilder()
 			.addComponents(
 				new ButtonBuilder()
-					.setCustomId('primary')
+					.setCustomId('attack')
+					.setLabel('Attack')
+					.setStyle(ButtonStyle.Danger)
+					.setDisabled(true),
+				
+				new ButtonBuilder()
+					.setCustomId('talk')
 					.setLabel('Talk')
 					.setStyle(ButtonStyle.Primary)
+					.setDisabled(true),
+				
+				new ButtonBuilder()
+					.setCustomId('items')
+					.setLabel('Item')
+					.setStyle(ButtonStyle.Secondary)
+					.setDisabled(true),
+				
+				new ButtonBuilder()
+					.setCustomId('flee')
+					.setLabel('Flee')
+					.setStyle(ButtonStyle.Success)
 					.setDisabled(true),
 			);
 
@@ -43,12 +76,36 @@ module.exports = {
 
 		await interaction.reply({ content: 'A Demon Appeared!', ephemeral: false, embeds: [embed], components: [row] });
 		
-		const filter = i => i.customId === 'primary' && i.user.id === '242118931769196544';
+		const filter = i => i.customId === 'talk' && i.user.id === '242118931769196544';
 
 		const collector = interaction.channel.createMessageComponentCollector({ filter, time: 15000 });
 
 		collector.on('collect', async i => {
-			await i.update({ content: 'You talked to the demon!', components: [] });
+			await i.update({ content: 'You talked to the demon!', components: [rowD] });
+		});
+		
+		const filter = j => j.customId === 'attack' && j.user.id === '242118931769196544';
+
+		const collector = interaction.channel.createMessageComponentCollector({ filter, time: 15000 });
+
+		collector.on('collect', async j => {
+			await j.update({ content: 'You attacked the demon!', components: [rowD] });
+		});
+		
+		const filter = k => k.customId === 'items' && k.user.id === '242118931769196544';
+
+		const collector = interaction.channel.createMessageComponentCollector({ filter, time: 15000 });
+
+		collector.on('collect', async k => {
+			await k.update({ content: 'You used an item', components: [rowD] });
+		});
+		
+		const filter = l => l.customId === 'flee' && l.user.id === '242118931769196544';
+
+		const collector = interaction.channel.createMessageComponentCollector({ filter, time: 15000 });
+
+		collector.on('collect', async l => {
+			await l.update({ content: 'You talked to the demon!', components: [rowD] });
 		});
 
 		collector.on('end', collected => console.log(`Collected ${collected.size} items`));
